5dfa887d042ebb7980d45a74c9c3c06a6367676c
James Cat <zz.james@gmail.com>
2012-09-03 15:36:34 +0100

    stuff working hella slow. focusing on camera now

--- resources/attributes/attributes.js
+++ resources/attributes/attributes.js
@@ -13,12 +13,11 @@ sb.attrib = {
 
     smorgs:
     {
-        block:sb.BlockSmorg,
-        cog7:sb.CogSmorg,
-        cog6:sb.CogSmorg,
-        cog5:sb.CogSmorg,
-        cog4:sb.CogSmorg
-
+        blok:{type:sb.BlockSmorg, params:{x:250,y:400, _width:780, _height: 20, rotation: 10}},
+        cog1:{type:sb.CogSmorg, params:{x:280,y:10}},
+        cog2:{type:sb.CogSmorg, params:{x:270,y:60}},
+        cog3:{type:sb.CogSmorg, params:{x:380,y:10}},
+        cog4:{type:sb.CogSmorg, params:{x:460,y:20}}
     },
 
     graphics: sb.Dhtml
