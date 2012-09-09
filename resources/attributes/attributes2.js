3308555bd8a73c1a8126d88361902c9e17ae1095
zz-james <zz.james@gmail.com>
2012-09-09 18:52:48 +0100

    demo 2 : done! alright - now to think about demo3. not bad!!!

--- resources/attributes/attributes2.js
+++ resources/attributes/attributes2.js
@@ -19,8 +19,9 @@ sb.attrib = {
         roof:{type:sb.BlockSmorg, params:{x:300,y:10, _width:600, _height: 20, rotation: +0}},
         floor:{type:sb.BlockSmorg, params:{x:300,y:390, _width:600, _height: 20, rotation: +0}},
         lwall:{type:sb.BlockSmorg, params:{x:10,y:200, _width:600, _height: 20, rotation: +90}},
-        rwall:{type:sb.BlockSmorg, params:{x:590,y:200, _width:960, _height: 20, rotation: +90}}
-
+        rwall:{type:sb.BlockSmorg, params:{x:590,y:200, _width:960, _height: 20, rotation: +90}},
+        cog2:{type:sb.CogSmorg, params:{x:350,y:60}},
+        cog3:{type:sb.CogSmorg, params:{x:60,y:250}}
     },
 
     graphics: sb.Dhtml
