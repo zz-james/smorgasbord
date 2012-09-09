55ec3de8e522b95e17e8cb69d05ccd7e16f74997
zz-james <zz.james@gmail.com>
2012-09-09 23:35:01 +0100

    demo 3 done. maybe spruce up with some interface i/o stuff if time. need to package and put onlline. test test test.

---
+++ resources/attributes/attributes3.js
@@ -0,0 +1,28 @@
+sb.attrib = {
+
+    game:
+    {
+        bord:sb.Bord2d2,
+        size:{_lox:0, _hix:600, _loy:0, _hiy:400},
+        viewport:{_lox:0, _hix:600, _loy:0, _hiy:400}
+    },
+
+    player:
+    {
+        name:'cog',
+        type:sb.MonkeySmorg,
+        params:{x:450,y:-250,density:20}
+    },
+
+    smorgs:
+    {
+        monkey:{type:sb.MonkeySmorg,params:{x:150,y:260,density:10}},
+        floor:{type:sb.BlockSmorg, params:{x:300,y:390, _width:600, _height: 20, rotation: +0}},
+        lwall:{type:sb.BlockSmorg, params:{x:10,y:200, _width:600, _height: 20, rotation: +90}},
+        rwall:{type:sb.BlockSmorg, params:{x:590,y:200, _width:960, _height: 20, rotation: +90}},
+        seesaw:{type:sb.SeeSawSmorg, params:{x:300,y:355, _width:400, _height: 20, rotation: +0}}
+    },
+
+    graphics: sb.Dhtml
+
+};
